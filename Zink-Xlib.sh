0e5b766c46a98f60ab90baa0650961d9d51ba844
JustCallMeJade <rotheory13@gmail.com>
2026-07-14 13:14:37 +0800

    Initialize Mesa installation script with Zink setup
    Script sets up the environment for Mesa installation, including necessary dependencies and configuration for Zink.

---
+++ Zink-Xlib.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+workdir="$(pwd)/workdir"
+install_dir="$workdir/install_dir"
+
+mkdir -p $workdir && mkdir -p $install_dir
+
+cd $workdir
+
+dnf update > /dev/null && dnf builddep mesa > /dev/null
+dnf install pkg-config wget cmake python3 git > /dev/null
+
+git clone --depth 1 https://gitlab.freedesktop.org/mesa/mesa.git
+
+cd mesa
+
+wget -O zink-experimental-patch.py https://raw.githubusercontent.com/JustCallMeJade/Extras/refs/heads/main/Extras/Py-Patches/zink-experimental.py
+
+python3 zink-experimental-patch.py
+
+meson setup build --prefix "$install_dir" -Dplatforms=x11 -Dglx=xlib -Dopengl=true -Dgles1=disabled -Degl=disabled -Dgles2=disabled -Dstrip=true -Dgallium-drivers=zink -Dvulkan-drivers=
+
+ninja -C build -j$(nproc) install
