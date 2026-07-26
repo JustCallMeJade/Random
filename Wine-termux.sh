a14883f9037b731fcbe7a8135eedc3ac186d4de6
JustCallMeJade <rotheory13@gmail.com>
2026-07-26 14:00:24 +0800

    Update Wine-termux.sh

--- Wine-termux.sh
+++ Wine-termux.sh
@@ -11,4 +11,9 @@ wget -O ndk.zip https://dl.google.com/android/repository/android-ndk-r29-linux.z
 unzip ndk.zip
 cd android-ndk-r29/toolchains/llvm/prebuilt/linux-x86_64/sysroot
 
-for patches in 1.patch 2.patch 3.patch 4.patch 5.patch 6.patch 7.patch 8.patch 9.patch 10.patch stdlib.h.patch sys-cdefs.h.patch sys-time.h.patch
+for patches in 1.patch 2.patch 3.patch 4.patch 5.patch 6.patch 7.patch 8.patch 9.patch 10.patch stdlib.h.patch sys-cdefs.h.patch sys-time.h.patch syslog.patch time.h.patch unistd.h.patch utmp.h.patch; do
+wget https://raw.githubusercontent.com/JustCallMeJade/termux-packages/refs/heads/master/ndk-patches/29/"$patches"
+patch -p1 -i "$patches"
+done
+
+cd ../../../../..
