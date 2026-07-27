b97cf40b553daf7a60f04568a1cc8e7a5d8e712f
JustCallMeJade <rotheory13@gmail.com>
2026-07-27 09:33:13 +0800

    Restore export commands for cross-compilation
    Re-add export commands for cross-compilation tools after configuration.

--- Wine-termux.sh
+++ Wine-termux.sh
@@ -36,15 +36,6 @@ wget https://raw.githubusercontent.com/JustCallMeJade/tur/refs/heads/master/tur/
 python3 patch-fixer.py "$patches"
 done
 
-export CROSSCC="arm64ec-w64-mingw32-clang"
-export CROSSCXX="arm64ec-w64-mingw32-clang++"
-export CC=$NDK/aarch64-linux-android30-clang
-export CXX=$NDK/aarch64-linux-android30-clang++
-export AR="$NDK/llvm-ar"
-export RANLIB="$NDK/llvm-ranlib"
-export STRIP="$NDK/llvm-strip"
-export NM="$NDK/llvm-nm"
-
 chmod +x configure
 
 mkdir wine-tools
@@ -55,6 +46,15 @@ cd wine-tools
 make -j$(nproc) __tooldeps__ nls/all
 
 cd ..
+
+export CROSSCC="arm64ec-w64-mingw32-clang"
+export CROSSCXX="arm64ec-w64-mingw32-clang++"
+export CC=$NDK/aarch64-linux-android30-clang
+export CXX=$NDK/aarch64-linux-android30-clang++
+export AR="$NDK/llvm-ar"
+export RANLIB="$NDK/llvm-ranlib"
+export STRIP="$NDK/llvm-strip"
+export NM="$NDK/llvm-nm"
 
 ./configure \
 --prefix="$OUTPUTDIR" \
